39c10ad16af08d5592429eaedae09288a3daffc7
MohitBhasin17 <bhasinm1791@gmail.com>
2024-07-22 16:22:43 +0530

    Add files via upload

---
+++ Assignment-40_3x3_Matrix.py
@@ -0,0 +1,22 @@
+#Write a NumPy program to create a 3x3 matrix with values ranging from 2 to 10.
+
+import numpy as np
+
+# Create a 3x3 matrix with values ranging from 2 to 10
+matrix = np.array([[2, 3, 4], 
+                   [5, 6, 7], 
+                   [8, 9, 10]])
+
+# Display the matrix
+print("3x3 matrix with values ranging from 2 to 10:")
+print(matrix)
+
+
+'''
+Output:-
+3x3 matrix with values ranging from 2 to 10:
+[[ 2  3  4]
+ [ 5  6  7]
+ [ 8  9 10]]
+
+'''
